a1c97ca0d914504a4e74e3907d470986a881993e
Prashant Sharma <prashantgoutam2001@gmail.com>
2025-10-14 00:00:13 +0530

    Included VAriable as well

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,2 +1,23 @@
+import math
 print('Hello World')
 print('My name is ChatGPT')
+#Modulo and "Modulus" operator
+print(10 % 3) # Outputs 1 because 10 divided by 3 leaves
+# a remainder of 1
+print(10 % 2) # Outputs 0 because 10 divided by 2 leaves
+
+print(2 ** 8)
+# Outputs 256 because 2 to the power of 8 is 256
+print(6*6)
+
+#Variable Assignment
+#Example
+#Python uses Dynamic Typing
+#this means you can reassign variables to diff data types.
+#This makes python very flexible in assigning datatypes, this is different than other language that are staticaly typed
+#Example
+my_dogs = 2
+#my_dogs = ["Sammy", "Frankie"]
+print(my_dogs)
+#print(my_dogs + my_dogs)
+print(type(my_dogs))
